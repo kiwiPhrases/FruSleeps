05bf8c5da2c8a9761951ca265e44ee25ca5baee0
kiwiphrases <gene.burinsky@gmail.com>
2023-12-28 00:25:30 -0500

    restyled everything except the login and registration forms

--- sleepdash.py
+++ sleepdash.py
@@ -5,6 +5,7 @@ from werkzeug.exceptions import abort
 
 import plotly
 import plotly.express as px
+from plotly.graph_objs import *
 import pandas as pd
 import json
 
@@ -27,18 +28,34 @@ def sleepdash():
 
     df.loc[:,'hour'] = df.wholetime.dt.hour
     df.loc[:,'minutes'] = df.wholetime.dt.minute
-    df.loc[:,'time'] = df.hour*60+df.minutes/60
+    df.loc[:,'time'] = df.hour*60+df.minutes
 
     # summarize data
     summary = df.groupby('parent').time.mean().reset_index()
     summary.loc[:,'time'] = summary.time/60
-
-    # Create Dash Layout
-    #figure=px.bar(summary,x='parent',y='time')),
-    #figure=px.line(x=df.wholetime.dt.date, y=df.time/60,labels={'x': 'Date', 'y':'Time'}))
-    meanTime = px.bar(summary,x='parent',y='time')
-    longit = px.line(x=df.wholetime.dt.date, y=df.time/60,labels={'x': 'Date', 'y':'Time'})
     
+    # generate graphs
+    # for colors check out: https://plotly.com/python/builtin-colorscales/
+    meanTime = px.bar(summary,x='parent',y='time',
+        color_discrete_sequence= px.colors.sequential.Viridis,
+        labels={'x':'Parent','y':'Average Time of Day'})
+        
+    meanTime.update_layout(paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        font={'color':'rgba(255, 95, 31, .9)','size':14})
+
+    longit = px.line(x=df.wholetime.dt.date, y=df.time/60,
+        labels={'x': 'Date', 'y':'Time'},
+        color_discrete_sequence= px.colors.sequential.Viridis)
+
+    longit.update_layout(paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        font={'color':'rgba(255, 95, 31, .9)','size':14},
+        xaxis={
+            "type": 'date',
+            "tickformat": '%b %d, %Y',
+            "dtick": 86400000.0})
+
     bargraphJSON = json.dumps(meanTime, cls=plotly.utils.PlotlyJSONEncoder)
     timegraphJSON = json.dumps(longit, cls=plotly.utils.PlotlyJSONEncoder)
 
