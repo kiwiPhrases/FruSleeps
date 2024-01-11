dac09a63cb8b4f6caf431451bac0556f7d8bc035
kiwiphrases <gene.burinsky@gmail.com>
2024-01-10 22:01:52 -0500

    removed bs4, added uwsgi ini

--- FruSleeps/wsgi.py
+++ FruSleeps/wsgi.py
@@ -1,3 +1,11 @@
-from .FruSleeps import create_app
+#from FruSleeps import create_app
 
-app = create_app()
+
+
+import sys
+#sys.path.extend([WORKING_DIR_AND_PYTHON_PATHS])
+from flask.cli import ScriptInfo
+from FruSleeps import create_app
+import FruSleeps as app
+locals().update(ScriptInfo(create_app=create_app).load_app().make_shell_context())
+print("Python %s on %s\nApp: %s\nInstance: %s" % (sys.version, sys.platform, app.import_name, app.instance_path))
